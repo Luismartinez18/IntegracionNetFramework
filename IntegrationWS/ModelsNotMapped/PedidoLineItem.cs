b51b5ed10eda870f5a8cc43289bdd36de127731f
Edward Tejada <edward.tejada@bionuclear.com.do>
2021-01-07 10:55:21 -0400

    cambios

--- IntegrationWS/ModelsNotMapped/PedidoLineItem.cs
+++ IntegrationWS/ModelsNotMapped/PedidoLineItem.cs
@@ -15,7 +15,7 @@ namespace IntegrationWS.ModelsNotMapped
         public decimal Impuesto__c { get; set; }
         public decimal TotalPrice__c { get; set; }
         public string Producto__c { get; set; }
-        public DateTime Fecha__c { get; set; }
+        //public DateTime Fecha__c { get; set; }
         public string Integration_Dynamics_Id__c { get; set; }
         
     }
